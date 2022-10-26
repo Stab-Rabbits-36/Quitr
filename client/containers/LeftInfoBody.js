56ecea641de2f17ace55061852b58a6b238ed843
Katharine Angelopoulos <katharineangelopoulos@Katharines-Air.attlocal.net>
2022-10-26 13:05:37 -0500

    working on info

--- client/containers/LeftInfoBody.js
+++ client/containers/LeftInfoBody.js
@@ -7,14 +7,10 @@ function LeftInfoBody(props) {
   console.log('LEFTINFOPROPS', props);
   return (
     <div className="leftInfoBody">
-      {/* <h1>Left side</h1> 
-      {props.habit ? props.habit.habit_name : null} */}
         <div className='left-up'>
-          <QuitInfo habit={props.habit} />
           <UpcomingInfo />
         </div>
         <div className='up-down'>
-           {/* {props.fact ? props.fact.fact : null} */}
             <HealthBenefits day={props.habit} fact={props.fact} />
         </div>
      </div>
