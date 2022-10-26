b44d45c838c6430a2375a5ed2c75961b104c6cad
kcuevas <cuev73@live.com>
2022-10-25 20:00:34 -0400

    initial run build and start of login page building

--- client/containers/Login.js
+++ client/containers/Login.js
@@ -1,9 +1,38 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
+const [userName, setUserName] = useState('');
+const [userPassword, setUserPassword] = useState('')
 
 const Login = (props) => {
   return (
-    <h1>This is the login page.</h1>
+    <div id='login-body'>
+      <HeaderBar />
+        <form id='login-box' onSubmit={setUser}>
+          <span className='primary-text'>LOGIN</span>
+          <div id='login-boxes'>
+              <div>
+                <input type='username'
+                placeholder='username'
+                name='username'
+                className='login-input'
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                />
+              </div>
+              <div>
+                <input type='password'
+                placeholder='password'
+                name='password'
+                className='login-input'
+                value={userPassword}
+                onChange={(e) => setUserPassword(e.target.value)}
+                />
+              </div>
+          </div>
+
+        </form>
+
+    </div>
   )
 }
 
