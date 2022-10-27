7930f137ec2d860cddc8bc67a5894ccd2431c04a
kcuevas <cuev73@live.com>
2022-10-27 13:33:45 -0400

    run build

--- client/containers/ChallengeContainer.js
+++ client/containers/ChallengeContainer.js
@@ -8,7 +8,7 @@ import Badge from '../components/Badge'
 const ChallengeContainer = (props) => {
   const[badges, setUserBadges] = useState([]);//use effect => import useEffect and look up syntax
   //write function to check points and update state accordingly
-
+  console.log(badges);
   useEffect(() => {
     const imgArray = ['https://cdn.discordapp.com/attachments/1022845969076850760/1035215304084697148/acolyte-resize-crop.png', 'https://cdn.discordapp.com/attachments/1022845969076850760/1035216286227120208/padawan-resize-crop.png', 'https://cdn.discordapp.com/attachments/1022845969076850760/1035217454022004736/jedi-knight-resize-crop.png', 'https://cdn.discordapp.com/attachments/1022845969076850760/1035217454537916446/jedi-master-resize-crop.png'];
     const {points} = props.habit;
@@ -21,7 +21,7 @@ const ChallengeContainer = (props) => {
     }else if(points <=750){
       setUserBadges([imgArray[0], imgArray[1], imgArray[2], imgArray[3]])
     }
-  });
+  }, []);
   return(
     <div className='leftInfoBody'>
       <h2>Log Your Challenges</h2>
